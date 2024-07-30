6f2203c0b5bd5c8b4cdb5f1b640cdf880027b3d2
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2024-07-29 21:49:52 -0300

    First commit

---
+++ Charity-Connect-React/src/templates/App/App.jsx
@@ -0,0 +1,168 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './App.css'
+
+import test from "../../assets/images/charityConnect.png";
+import ods10 from "../../assets/images/ods1-nobackground-10.png";
+import ods11 from "../../assets/images/ods10-nobackground-11.png";
+import ods12 from "../../assets/images/ods12-nobackground-12.png";
+import ods13 from "../../assets/images/ods17-nobackground-13.png";
+import footer from "../../assets/images/newshortlogobranca-12.png";
+
+
+
+function App() {
+  return (/*
+    <div classNameName='container'>
+      <nav classNameName="navbar navbar-expand-lg menu">
+        <div classNameName="container-fluid">
+          <a classNameName="navbar-brand" href="#">Navbar</a>
+          <button classNameName="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span classNameName="navbar-toggler-icon"></span>
+          </button>
+          <div classNameName="collapse navbar-collapse" id="navbarNav">
+            <ul classNameName="navbar-nav">
+              <li classNameName="nav-item">
+                <a classNameName="nav-link" href="#">Home</a>
+              </li>
+              <li classNameName="nav-item">
+                <a classNameName="nav-link" href="#">Produtos</a>
+              </li>
+              <li classNameName="nav-item">
+                <a classNameName="nav-link" href="#">Quem Somos</a>
+              </li>
+              <li classNameName="nav-item">
+                <a classNameName="nav-link" href="#">Fale Conosco</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+   
+      <footer>
+        <Link to={'/login'}
+          classNameName='btn btn-sm btn-warning'>
+          Acesso Restrito
+        </Link>
+      </footer>
+    </div>
+  */
+    
+        <div className='background'>
+          <header id="cabecalho">
+            <a href="#" id="logo" target="_blank">
+              <img src={test} style={{ width: '16rem' }} alt="Logo" />
+            </a>
+            <nav id="nav">
+              <button id="btn-mobile">
+                <span id="hambuguer"></span>
+              </button>
+              <ul id="menu">
+                <li><a id="itens" href="#" target="_blank">Home</a></li>
+                <li><a id="itens" href="#" target="_blank">Catálogo</a></li>
+                <li><a id="itens" href="#" target="_blank">Meu Perfil ONG</a></li>
+                <li><a id="itens" href="#" target="_blank">Sobre nós</a></li>
+                <li>
+                  <a style={{ "--clr": "#707bff" }} className="btn-entrar" id="itens" href="#" target="_blank">
+                    <span>Entrar</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </header>
+    
+          <main className="mainhome">
+            <section className="parte1home">
+              <div className="titulo1home">
+                <h1 id="titulo1-1">Charity Connect.</h1>
+              </div>
+              <div className="teste">
+                <div className="doeaquihome">
+                  <p id="text-doe">Doe hoje!</p>
+                  <h3 id="desc-doe">
+                    Conectando corações solidários: Descubra ONGs com propósitos que você se identifica.
+                  </h3>
+                </div>
+              </div>
+            </section>
+    
+            <div className="div-gradiente-home"></div>
+    
+            <section className="onu">
+              <section className="container">
+                <h1> ODS</h1>
+                <div className="slider-wrapper">
+                  <div className="slider">
+                    <img id="slide-1" src={ods10} alt="ODS 1 Erradicação da Pobreza" />
+                    <img id="slide-2" src={ods11} alt="ODS 10 Redução das Desigualdades" />
+                    <img id="slide-3" src={ods12} alt="ODS 12 Consumo e Produção Responsáveis" />
+                    <img id="slide-4" src={ods13} alt="ODS 1 Parcerias e Meios de Implementação" />
+                  </div>
+                  <div className="slider-nav">
+                    <a href="#" target="_blank"></a>
+                    <a href="#" target="_blank"></a>
+                    <a href="#" target="_blank"></a>
+                    <a href="#" target="_blank"></a>
+                  </div>
+                </div>
+              </section>
+            </section>
+    
+            <section className="avaliacao">
+              <p id="text-avaliacao">
+                Charity Connect é um incrível site de <br />doações nacional! Eles proporcionam um<br /> ambiente caloroso e
+                acolhedor, facilitando a<br /> doação e a ajuda aos necessitados. Obrigado <br />por fazer a diferença em nossa
+                comunidade!
+              </p>
+              <h3 id="nome-doador">- Kanye West</h3>
+            </section>
+          </main>
+    
+          <footer className="footergeral">
+            <section className="footercontent">
+              <figure className="logofooter">
+                <img id="logofooter" src={footer} alt="Logo Footer" />
+                <h1 id="text-logo-footer">Charity Connect.</h1>
+              </figure>
+    
+              <div className="footerpart2">
+                <a id="link-footer-pt2" href="#" target="_blank">Sobre o Projeto</a>
+                <a id="link-footer-pt2" href="#" target="_blank">Contato</a>
+              </div>
+    
+              <div className="footerpart3">
+                <a id="link-footer-pt3" href="#" target="_blank">Política de Privacidade</a>
+                <a id="link-footer-pt3" href="#" target="_blank">Termos de Uso</a>
+              </div>
+    
+              <div className="divisaodofooter2">
+                <div className="origemdotrabalho">
+                  <div className="integrantesdiv">
+                    <h2 id="itensfooter2">Integrantes:
+                      <li id="itemfooter">02 Cauã Santana</li>
+                      <li id="itemfooter">07 Gustavo Alves</li>
+                      <li id="itemfooter">08 Gustavo Gomes</li>
+                      <li id="itemfooter">13 Leandro Afonso Niches</li>
+                      <li id="itemfooter">23 Nathalia Lima</li>
+                      <li id="itemfooter">28 Ruan Pablo Braga Mendes</li>
+                      <li id="itemfooter">32 Wellington Cavalcante</li>
+                    </h2>
+                  </div>
+                  <div className="profediscfooter">
+                    <h2 id="itensfooter2">ITB - FIEB
+                      <li id="itemfooter">Brasílio Flores de Azevedo</li>
+                    </h2>
+                    <h2 id="itensfooter2">Curso
+                      <li id="itemfooter">Informática 2022 - 2024</li>
+                    </h2>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </footer>
+        </div>
+      )
+    }
+    
+
+export default App
