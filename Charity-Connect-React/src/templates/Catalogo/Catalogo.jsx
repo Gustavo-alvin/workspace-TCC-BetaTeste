a034fe28480a158ed6bf8dd929ee5beac5ad38df
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2024-08-01 07:52:24 -0300

    kkkkk

--- Charity-Connect-React/src/templates/Catalogo/Catalogo.jsx
+++ Charity-Connect-React/src/templates/Catalogo/Catalogo.jsx
@@ -4,13 +4,13 @@ import Sidebar from '../../components/Menu/Sidebar'
 import './Catalogo.css'
 // import './Reset.css'
 import "../Catalogo/Reset.css"
-import ong2 from "../../assets/images/ongs-02.png"
-import ong3 from "../../assets/images/ongs-03.png"
-import ong4 from "../../assets/images/ongs-04.png"
-import ong5 from "../../assets/images/ongs-05.png"
-import ong6 from "../../assets/images/ongs_Prancheta 1.png"
-import ong7 from "../../assets/images/ongs-06.png"
-import ong8 from "../../assets/images/ongs-08.png"
+import ong2 from "../../assets/images/ongs-02.png";
+import ong3 from "../../assets/images/ongs-03.png";
+import ong4 from "../../assets/images/ongs-04.png";
+import ong5 from "../../assets/images/ongs-05.png";
+import ong6 from "../../assets/images/ongs_Prancheta 1.png";
+import ong7 from "../../assets/images/ongs-06.png";
+import ong8 from "../../assets/images/ongs-08.png";
 
 
 const Catalogo = () => {
@@ -18,7 +18,7 @@ const Catalogo = () => {
     return (
         <div>
             <header id="cabecalho">
-        <a href="../html/index.html" id="logo"><img src="../assets/charityConnect.png" style={{width: "16rem;"}} alt=""/></a>
+        <a href="../html/index.html" id="logo"><img src="../assets/charityConnect.png" style={{"width": "16rem"}} alt=""/></a>
         <nav id="nav">
           <button id="btn-mobile">
             <span id="hambuguer"></span>
@@ -28,7 +28,7 @@ const Catalogo = () => {
             <li><a id="itens" href={'/Catalogo'}>Catálogo</a></li>
             <li><a id="itens" href="#">Meu Perfil ONG</a></li>
             <li><a id="itens" href="#">Sobre nós</a></li>
-            <li><a style={{"--clr":"#707bff"}} classNameNameName="btn-entrar" id="itens" href="../html/login.html"><span>Entrar</span></a>
+            <li><a style={{"--clr":"#707bff"}} className="btn-entrar" id="itens" href="../html/login.html"><span>Entrar</span></a>
             </li>
           </ul>
         </nav>
@@ -36,13 +36,13 @@ const Catalogo = () => {
                 
 
       <section>
-        <div classNameName="input-wrapper">
-            <label for="filter" classNameName="sr-only" >Pesquisar</label>
+        <div className="input-wrapper">
+            <label htmlFor="filter" className="sr-only" >Pesquisar</label>
             <input id="filter" type="text" placeholder="Pesquiar" />
         </div>
 
-        <div classNameName="category-filter">
-            <div classNameName="select-wrapper-city">
+        <div className="category-filter">
+            <div className="select-wrapper-city">
               <select id="interestsSelect-city">
                 <option value="">Selecione uma Cidade</option>
                 <option value="jandira">Jandira</option>
@@ -53,7 +53,7 @@ const Catalogo = () => {
             </div>
       
       
-            <div classNameName="select-wrapper">
+            <div className="select-wrapper">
               <select id="interestsSelect">
                 <option value="">Selecione um Interesse</option>
                 <option value="roupa">Roupas</option>
@@ -70,7 +70,7 @@ const Catalogo = () => {
             
             <li className="items" data-interests="monetaria" data-city="barueri">
                 <div className="img-ong">
-                    <img src={{ong2}} alt=""/>
+                    <img src={ong2} alt="sorrisos do amanha "/>
                 </div>
                 <div className="info-ongs">
                     <h2>Sorrisos do Amanhã</h2>
@@ -83,7 +83,7 @@ const Catalogo = () => {
             
             <li className="items" data-interests="roupas" data-city="jandira">
                 <div className="img-ong">
-                    <img src={{ong3}} alt=""/>
+                    <img src={ong3} alt="logo maos solidaria "/>
                 </div>
                 <div className="info-ongs">
                     <h2>Mãos Solidárias</h2>
@@ -96,7 +96,7 @@ const Catalogo = () => {
              
             <li className="items" data-interests="brinquedo" data-city="osasco">
                 <div className="img-ong">
-                    <img src={{ong4}} alt=""/>
+                    <img src={ong4} alt="logo amigos do bem estar"/>
                 </div>
                 <div className="info-ongs">
                     <h2>Amigos do Bem-Estar</h2>
@@ -107,7 +107,7 @@ const Catalogo = () => {
             
             <li className="items" data-interests="alimento" data-city="carapicuiba">
                 <div className="img-ong">
-                    <img src={{ong5}} alt=""/>
+                    <img src={ong5} alt="Esperança em açao"/>
                 </div>
                 <div className="info-ongs">
                     <h2>Esperança em Ação</h2>
@@ -118,7 +118,7 @@ const Catalogo = () => {
             
              <li className="items" data-interests="alimento" data-city="barueri">
                 <div className="img-ong">
-                    <img src={{ong6}} alt=""/>
+                    <img src={ong6} alt="coraçao generoso"/>
                 </div>
                 <div className="info-ongs">
                     <h2>Coração Generoso</h2>
@@ -129,7 +129,7 @@ const Catalogo = () => {
            
             <li className="items" data-interests="roupa" data-city="carapicuiba">
                 <div className="img-ong">
-                    <img src={{ong7}} alt=""/>
+                    <img src={ong7} alt="logo junto pela infancia"/>
                 </div>
                 <div className="info-ongs">
                     <h2>Juntos pela Infância</h2>
@@ -140,7 +140,7 @@ const Catalogo = () => {
             
              <li className="items" data-interests="brinquedo" data-city="jandira">
                 <div className="img-ong">
-                    <img className="logo-ongs" src="../assets/ongs-07.png" alt=""/>
+                    <img className="logo-ongs" src="../assets/ongs-07.png" alt="logo vida renovada"/>
                 </div>
                 <div className="info-ongs">
                     <h2>Vida Renovada</h2>
@@ -151,7 +151,7 @@ const Catalogo = () => {
             
              <li className="items" data-interests="alimento" data-city="osasco">
                 <div className="img-ong">
-                    <img src={{ong8}} alt=""/>
+                    <img src={ong8} alt="logo rede solidaridade"/>
                 </div>
                 <div className="info-ongs">
                     <h2>Rede da Solidariedade</h2>
