160fbb8be9793d2afd5adab9bd89a458ad1d887b
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2024-08-02 11:07:36 -0300

    kkk

--- Charity-Connect-React/src/templates/Cadastro/Cadastro.jsx
+++ Charity-Connect-React/src/templates/Cadastro/Cadastro.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 import Header from "../../components/Header/Header"
 import Sidebar from '../../components/Menu/Sidebar'
 
-const Home = () => {
+const Catalogo = () => {
 
     
 
