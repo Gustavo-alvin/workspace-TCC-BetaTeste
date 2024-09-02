49020bd1bd8f1b52ee1c36d480872ec533d94ec2
Cauã Santana <114744734+emostalooknegativeqi@users.noreply.github.com>
2024-09-02 12:04:10 -0300

    alterações no login

--- Charity-Connect-React/src/services/LoginService.js
+++ Charity-Connect-React/src/services/LoginService.js
@@ -1,41 +1,33 @@
 import http from '../common/http-common';
 
-// Defina o URL da API base para as operações relacionadas a ONGs
-const API_URL = "ong/";
+const API_URL = "ong/signin";
 
-// Função para autenticar o usuário
 const signin = async (email, senha) => {
-    try {
-        const response = await http.mainInstance.post(`${API_URL}signin`, {
+    const response = await http.mainInstance
+        .post(API_URL + "signin", {
             email,
-            senha
+            senha,
         });
-        if (response.data) {
-            localStorage.setItem("ong", JSON.stringify(response.data));
-        }
-        return response.data;
-    } catch (error) {
-        console.error("Erro ao fazer login:", error);
-        throw error;
+    if (response.data) {
+        localStorage.setItem("user", JSON.stringify(response.data));
     }
+    return response.data;
 };
 
-// Função para deslogar o usuário
 const logout = () => {
     localStorage.removeItem("ong");
 };
 
-// Função para obter os dados do usuário autenticado do localStorage
 const getCurrentOng = () => {
     return JSON.parse(localStorage.getItem("ong"));
 };
 
-// Função para verificar se o usuário está autenticado
+
 const isAuthenticated = () => {
     return !!getCurrentOng();
 };
 
-// Função para criar uma nova ONG (ajuste conforme necessário)
+
 const create = async (data) => {
     try {
         const response = await http.mainInstance.post(`${API_URL}create`, data);
@@ -46,7 +38,6 @@ const create = async (data) => {
     }
 };
 
-// Função para listar todas as ONGs
 const findAll = async () => {
     try {
         const response = await http.mainInstance.get(`${API_URL}findAll`);
@@ -57,7 +48,6 @@ const findAll = async () => {
     }
 };
 
-// Função para buscar uma ONG por ID
 const findById = async (id) => {
     try {
         const response = await http.mainInstance.get(`${API_URL}findById/${id}`);
@@ -68,7 +58,7 @@ const findById = async (id) => {
     }
 };
 
-// Exporta o serviço para uso em outros componentes
+
 const LoginService = {
     signin,
     logout,
