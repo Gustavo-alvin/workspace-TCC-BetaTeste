5f6e38824e24c30bd84a34162de28cb873944d11
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2024-10-06 23:02:54 -0300

    quero morrerrr

--- Charity-Connect-React/src/templates/Perfil/EditarPerfil.jsx
+++ Charity-Connect-React/src/templates/Perfil/EditarPerfil.jsx
@@ -4,251 +4,233 @@ import fotoong from "../../assets/images/ongs-02.png";
 import footer from "../../assets/images/newnames_Prancheta 1.png";
 import ong2 from "../../assets/images/ongs-02.png";
 import "./EditarPerfil.css";
-import { useParams } from "react-router-dom";
-import CatalogoService from "../../services/CatalogoService";
+import { useParams, useNavigate } from "react-router-dom"; // Importar useNavigate para redirecionamento
+import CadastroService from "../../services/CadastroService";
 import MenuBar from "../../components/Menu/MenuBar";
 import Footer from "../../components/Footer/Footer";
-import catalogo from "../Catalogo/Catalogo"
-import CadastroService from "../../services/CadastroService";
 
 function EditarPerfil() {
+  const { id } = useParams();
+  const navigate = useNavigate(); // Para redirecionamento após a atualização
 
-  const [dataFile, setDataFile] = useState();
-  const [chosenImage, setChosenImage] = useState();
+  const objectValues = {
+    id: null,
+    nome: "",
+    nomeRep: null,
+    email: null,
+    telefone: null,
+    descAtuacao: null,
+    foto: null,
+    cep: null,
+    cnpj: null,
+    uf: null,
+    localidade: null,
+    endereco: null,
+    bairro: null,
+    cidade: null,
+    interesse: null,
+  };
+
+  const [cadastro, setCadastro] = useState(objectValues);
+  const [loading, setLoading] = useState(true);
   const [formData, setFormData] = useState({});
-  const [confirmPassword, setConfirmPassword] = useState(""); // Novo estado para confirmação da senha
-  const [successful, setSuccessful] = useState(false);
-  const [message, setMessage] = useState();
-  const [input, setInput] = useState("");
-  const [cep, setCep] = useState({});
 
-  async function handleSearch() {
-    if (input === "") {
-      alert("Digite algum CEP!");
-      return;
-    }
-
-    try {
-      const response = await api.get(`${input}/json`);
-      setCep(response.data);
-      setInput("");
-    } catch {
-      alert("Opa! Tem algum erro aí");
-      setInput("");
-    }
-  }
-
-  const setFile = (dataFile) => {
-    setDataFile(dataFile);
-  };
-
-  const setImage = (dataImage) => {
-    setChosenImage(dataImage);
-  };
+  useEffect(() => {
+    CadastroService.findById(id)
+      .then((response) => {
+        const cadastro = response.data;
+        setCadastro(cadastro);
+        setLoading(false);
+        console.log(cadastro);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, [id]);
 
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
+    setFormData(formData => ({ ...formData, [name]: value }));
+}
 
-    if (e.target.type === "file") {
-      setFormData((formData) => ({ ...formData, [name]: e.target.files[0] }));
-    } else {
-      setFormData((formData) => ({ ...formData, [name]: value }));
-    }
-    console.log(formData);
-    // Atualiza o estado de confirmação da senha
-    if (name === "senha1") {
-      setConfirmPassword(value);
-    }
-  };
-
-  const handleReset = () => {
-    setFormData({
-      nome: "",
-      nomeRep: "",
-      email: "",
-      cnpj: "",
-      telefone: "",
-      cidade: "",
-      bairro: "",
-      file: "",
-      endereco: "",
-      interesse: "",
-      cep: "",
-      descAtuacao: "",
-      senha: "",
-    });
-    setConfirmPassword(""); // Reseta a confirmação da senha
-    setDataFile(null);
-    setChosenImage(null);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setSuccessful(false);
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Impede o comportamento padrão do formulário
     console.log("Dados do formulário:", formData);
 
-    CadastroService.create(dataFile, formData).then(
-      (response) => {
-        setMessage(response.data.message);
-        setSuccessful(true);
-      },
-      (error) => {
-        const message = error.response.data.message;
-        setMessage(message);
-      }
-    );
+    CadastroService.update(id, formData)
+      .then(() => {
+        console.log("Dados atualizados com sucesso!");
+        navigate("/perfil"); // Redirecionar para a página de perfil após a atualização
+      })
+      .catch((error) => {
+        console.error("Erro ao atualizar os dados:", error);
+      });
   };
 
-  const [data, setData] = useState("");
-  const setChosenCep = (dataFile) => {
-    setData(dataFile);
-  };
-  useEffect(() => {
-    if (data) setCep(data);
-    setFormData((formData) => ({ ...formData, data }));
-  }, [data]);
-
+  if (loading) {
+    return <div>Carregando...</div>;
+  }
 
   return (
     <div>
-       <MenuBar />
+      <MenuBar />
 
-       <main className="editar">
+      <main className="editar">
+        <div className="coracaocad">
+          <figure className="coracaofigure">
+            <img id="coracao" alt="Coração" />
+          </figure>
+        </div>
 
-<div className="coracaocad">
-    <figure className="coracaofigure">
-        <img id="coracao" />
-    </figure>
-</div>
+        <section className="kk">
+          <form className="form" onSubmit={handleSubmit}>
+            <div className="divisao-inputs">
+              <label htmlFor="nome">
+                <h1>NOME DA ORGANIZAÇÃO:</h1>
+                <input
+                  className="editar-inputs"
+                  type="text"
+                  name="nome"
+                  defaultValue={cadastro.nome}
+                  onChange={handleChange} // Atualiza o estado conforme o usuário digita
+                />
+              </label>
 
+              <label htmlFor="nomeRep">
+                <h1>NOME DO REPRESENTANTE:</h1>
+                <input
+                  className="editar-inputs"
+                  type="text"
+                  name="nomeRep"
+                  value={cadastro.nomeRep}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
 
-<section className="kk">
-    
-            <form onSubmit={handleSubmit} className="form" method="post" action="">
-    
+            <div className="divisao-inputs-solos">
+              <label htmlFor="email">
+                <h1>EMAIL DA ORGANIZAÇÃO:</h1>
+                <input
+                  className="editar-inputs-solos"
+                  type="email"
+                  name="email"
+                  value={cadastro.email}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
 
-                   <div className="divisao-inputs">
+            <div className="divisao-inputs">
+              <label htmlFor="telefone">
+                <h1>NÚMERO DE CELULAR:</h1>
+                <input
+                  className="editar-inputs"
+                  type="text"
+                  name="telefone"
+                  value={cadastro.telefone}
+                  onChange={handleChange}
+                />
+              </label>
 
-                        <label htmlFor="">
-                            <h1>NOME DA ORGANIZAÇÃO:</h1>
-                            <input className="editar-inputs" type="text"
-                            name="nome"
-                            value={formData.nome || ""}
-                            onChange={handleChange} />
-                        </label>
+              <label htmlFor="cep">
+                <h1>CEP:</h1>
+                <input
+                  className="editar-inputs"
+                  type="text"
+                  name="cep"
+                  value={cadastro.cep}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
 
-                        <label htmlFor="">
-                            <h1>NOME DO REPRESENTANTE:</h1>
-                            <input className="editar-inputs" type="text"
-                             name="nomeRep"
-                             value={formData.nomeRep || ""}
-                             onChange={handleChange} />
-                        </label>
-                   </div>
+            <div className="divisao-inputs-solos">
+              <label htmlFor="interesse">
+                <h1>INTERESSES:</h1>
+                <input
+                  className="editar-inputs-solos"
+                  type="text"
+                  name="interesse"
+                  value={cadastro.interesse}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
 
-                   <div className="divisao-inputs-solos">
-                    <label htmlFor="">
-                        <h1>EMAIL DA ORGANIZAÇÃO:</h1>
-                        <input className="editar-inputs-solos" type="email"
-                        name="email"
-                         value={formData.email || ""}
-                         onChange={handleChange} />
-                    </label>
-                   </div>
+            <div className="divisao-inputs">
+              <label htmlFor="endereco">
+                <h1>Endereço</h1>
+                <input
+                  className="editar-inputs"
+                  type="text"
+                  name="endereco"
+                  value={cadastro.endereco}
+                  onChange={handleChange}
+                />
+              </label>
 
-                   <div className="divisao-inputs">
+              <label htmlFor="bairro">
+                <h1>Bairro</h1>
+                <input
+                  className="editar-inputs"
+                  type="text"
+                  name="bairro"
+                  value={cadastro.bairro}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
 
-                        <label htmlFor="">
-                            <h1>NÚMERO DE CELULAR::</h1>
-                            <input className="editar-inputs" type="text"
-                            name="telefone"
-                            value={formData.telefone || ""}
-                            onChange={handleChange} />
-                        </label>
+            <div className="divisao-inputs">
+              <label htmlFor="cidade">
+                <h1>CIDADE:</h1>
+                <input
+                  className="editar-inputs"
+                  type="text"
+                  name="cidade"
+                  value={cadastro.cidade}
+                  onChange={handleChange}
+                />
+              </label>
 
-                        <label htmlFor="">
-                            <h1>CEP:</h1>
-                            <input className="editar-inputs" type="text"
-                            name="cep" />
-                        </label>
-                   </div>
+              <label htmlFor="cnpj">
+                <h1>CNPJ:</h1>
+                <input
+                  className="editar-inputs"
+                  type="text"
+                  name="cnpj"
+                  value={cadastro.cnpj}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
 
+            <div className="divisao-inputs-solos">
+              <label htmlFor="descAtuacao">
+                <h1>DESCRIÇÃO DE ATUAÇÃO:</h1>
+                <textarea
+                  name="descAtuacao"
+                  className="editar-inputs-solos"
+                  cols="40"
+                  rows="6"
+                  maxLength="200"
+                  value={cadastro.descAtuacao}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
 
-                   <div className="divisao-inputs-solos">
-                    <label htmlFor="">
-                        <h1>INTERESSES:</h1>
-                        <input className="editar-inputs-solos" type="text" 
-                        name="interesse"
-                        value={formData.interesse || ""}
-                        onChange={handleChange}/>
-                    </label>
-                   </div>
-
-                     <div className="divisao-inputs">
-
-                        <label htmlFor="">
-                            <h1>Endereço</h1>
-                            <input className="editar-inputs" type="text"
-                            name="endereco"
-                            value={formData.endereco || ""}
-                            onChange={handleChange} />
-                        </label>
-
-                        <label htmlFor="">
-                            <h1>Bairro</h1>
-                            <input className="editar-inputs" type="text" 
-                            name="endereco"
-                            value={formData.endereco || ""}
-                            onChange={handleChange}/>
-                        </label>
-                   </div>
-
-                     <div className="divisao-inputs">
-
-                        <label htmlFor="">
-                            <h1>CIDADE:</h1>
-                            <input className="editar-inputs" type="text"
-                            name="cidade"
-                            value={formData.cidade || ""}
-                            onChange={handleChange} />
-                        </label>
-
-                        <label htmlFor="">
-                            <h1>CNPJ:</h1>
-                            <input className="editar-inputs" type="text"
-                            name="cnpj"
-                            value={formData.cnpj || ""}
-                            onChange={handleChange} />
-                        </label>
-                   </div>
-
-                   <div className="divisao-inputs-solos">
-                    <label htmlFor="">
-                        <h1>DESCRIÇÃO DE ATUAÇÃO:</h1>
-                        <textarea name="descAtuacao" className="editar-inputs-solos" id=""
-                        textarea
-                        value={formData.descAtuacao || ""}
-                        cols="40"
-                        rows="6"
-                        maxLength="200"
-                        onChange={handleChange}>
-
-                        </textarea>
-                        
-                    </label>
-                   </div>
-    
-                
-
-                   <div className="confirmarcao-perfil">
+            <div className="confirmarcao-perfil">
               <div className="btns-do-perfil">
                 <button
                   className="botao-form-perfil"
                   id="botao-form"
                   type="submit"
                 >
-                 Salvar
+                  Salvar
                 </button>
               </div>
               <div div className="btns-do-perfil">
@@ -256,28 +238,18 @@ function EditarPerfil() {
                   className="botao-form-perfi"
                   id="botao-form"
                   type="button"
+                  onClick={() => setCadastro(objectValues)} // Limpar o formulário
                 >
                   Limpar
                 </button>
               </div>
             </div>
-
-
-            </form>
-                    
+          </form>
         </section>
+      </main>
 
-                
-
-    
-            
-
-
-</main>
-
-       <Footer/>
-
-      </div>
+      <Footer />
+    </div>
   );
 }
 
--- Charity-Connect-React/src/templates/Perfil/Perfil.jsx
+++ Charity-Connect-React/src/templates/Perfil/Perfil.jsx
@@ -4,13 +4,13 @@ import fotoong from "../../assets/images/ongs-02.png";
 import footer from "../../assets/images/newnames_Prancheta 1.png";
 import ong2 from "../../assets/images/ongs-02.png";
 import "./Perfil.css";
-import { useParams, useNavigate } from "react-router-dom"; // Import useNavigate
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import CatalogoService from "../../services/CatalogoService";
 import MenuBar from "../../components/Menu/MenuBar";
 
 function Perfil() {
   const { id } = useParams();
-  const navigate = useNavigate(); // Initialize navigate
+  const navigate = useNavigate(); // Inicialize o hook useNavigate
 
   const objectValues = {
     id: null,
@@ -41,46 +41,34 @@ function Perfil() {
       });
   }, [id]);
 
-  // Function to handle edit button click
-  const handleEditClick = (id) => {
-    navigate(`/editarperfil/` + id); // Redirect to the edit profile page
+  const goToEditarPerfil = () => {
+    navigate(`/editarperfil/` + id); // Use navigate() para redirecionar
   };
 
   return (
     <div>
-      <MenuBar />
+       <MenuBar />
 
       <main className="minhaONg">
-        <form action="" className="formulario">
-          <div className="perfil-principal">
-            <section className="sessao-perfil">
-              <div className="teste">
-                <img
-                  id="foto-perfil"
-                  src={catalogo.foto ? "data:image/jpeg;base64," + catalogo.foto : ""}
-                  alt=""
-                />
-              </div>
-              <div>
-                <span id="nome-ong">{catalogo.nome}</span>
-                <br />
-                <label className="desc" htmlFor="">
-                  {catalogo.descAtuacao}
-                </label>
-              </div>
-              <div>
-                <button
-                  type="button"
-                  id="botaocatalogo"
-                  className="editar"
-                  onClick={handleEditClick} // Add onClick event to redirect
-                >
-                  Editar
-                </button>
-              </div>
-            </section>
+      <form action="" className="formulario">
+        <div className="perfil-principal">
+          <section className="sessao-perfil">
+            <div className="teste">
+              <img id="foto-perfil" src={catalogo.foto ? 'data:image/jpeg;base64,' + catalogo.foto : ""} alt="" />
+            </div>
+            <div>
+              <span id="nome-ong">{catalogo.nome}</span>
+              <br />
+             <label className="desc" htmlFor="">{catalogo.descAtuacao}</label>
+            </div>
+            <div>
+              <button type="button" onClick={goToEditarPerfil} id="botaocatalogo" className="editar">
+                Editar
+              </button>
+            </div>
+          </section>
 
-            <section className="info-ong">
+          <section className="info-ong">
             
               <div className="info">
                 <label htmlFor="id" className="label-input">
@@ -123,13 +111,25 @@ function Perfil() {
                 <label htmlFor="id" className="label-input">
                 UF:
                 </label>
-                <label className="area-contato" htmlFor="">{catalogo.uf}</label>
+                <label className="area-contato" htmlFor="">{`${catalogo.cidade} - ${catalogo.uf}`}</label>
               </div>
+              <div className="info">
+                <label htmlFor="id" className="label-input">
+                Bairro:
+                </label>
+                <label className="area-contato" htmlFor="">{catalogo.bairro}</label>
+              </div>
+              <div className="info">
+                <label htmlFor="id" className="label-input">
+                Endereço:
+                </label>
+                <label className="area-contato" htmlFor="">{catalogo.endereco}</label>
+              </div>
+              
               
             
           </section>
-          
-          </div>
+        </div>
         </form>
       </main>
 
@@ -139,7 +139,52 @@ function Perfil() {
             <img id="logofooter" src={footer} alt="Logo Footer" />
             <h1 id="text-logo-footer">Charity Connect.</h1>
           </figure>
-          {/* The rest of your footer */}
+
+          <div className="footerpart2">
+            <a id="link-footer-pt2" href={"/sobre"}>
+              Sobre o Projeto
+            </a>
+            <a id="link-footer-pt2" href={"/contato"}>
+              Contato
+            </a>
+          </div>
+
+          <div className="footerpart3">
+            <a id="link-footer-pt3" href={"/politicas"}>
+              Política de Privacidade
+            </a>
+            <a id="link-footer-pt3" href={"/termos"}>
+              Termos de Uso
+            </a>
+          </div>
+
+          <div className="divisaodofooter2">
+            <div className="origemdotrabalho">
+              <div className="integrantesdiv">
+                <h2 id="itensfooter2">
+                  Integrantes:
+                  <li id="itemfooter">02 Cauã Santana</li>
+                  <li id="itemfooter">07 Gustavo Alves</li>
+                  <li id="itemfooter">08 Gustavo Gomes</li>
+                  <li id="itemfooter">13 Leandro Afonso Niches</li>
+                  <li id="itemfooter">23 Nathalia Lima</li>
+                  <li id="itemfooter">28 Ruan Pablo Braga Mendes</li>
+                  <li id="itemfooter">32 Wellington Cavalcante</li>
+                  <li id="itemfooter">32 Vinicius de Jesus Barreto</li>
+                </h2>
+              </div>
+              <div className="profediscfooter">
+                <h2 id="itensfooter2">
+                  ITB - FIEB
+                  <li id="itemfooter">Brasílio Flores de Azevedo</li>
+                </h2>
+                <h2 id="itensfooter2">
+                  Curso
+                  <li id="itemfooter">Informática 2022 - 2024</li>
+                </h2>
+              </div>
+            </div>
+          </div>
         </section>
       </footer>
     </div>
