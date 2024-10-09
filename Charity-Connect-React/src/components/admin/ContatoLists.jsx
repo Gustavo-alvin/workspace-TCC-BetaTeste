063a3eeb6dbed8605aad1ab73af186e668e5c6dc
Cauã Santana <114744734+emostalooknegativeqi@users.noreply.github.com>
2024-10-08 22:02:16 -0300

    alguma alterações no contato admin

--- Charity-Connect-React/src/components/admin/ContatoLists.jsx
+++ Charity-Connect-React/src/components/admin/ContatoLists.jsx
@@ -6,11 +6,19 @@ const ContatoList = ({ contatos }) => {
   return (
     <div>
       {contatos.map((contato) => (
-        <div key={contato.id} className="admin-header">
-          <p><strong>Data</strong>: {contato.dataContato}</p>
-          <p><strong>Motivo</strong>: {contato.motivoContato}</p>
-          <p><strong>Pergunta</strong>: {contato.pergunta}</p>
-          {/* outros detalhes do contato */}
+        <div key={contato.id} className="admin-header p-3 my-4 shadow">
+          <div className="1slot shadow">
+            <p><strong>Data</strong>: {contato.dataContato}</p>
+            <p><strong>Motivo</strong>: {contato.motivoContato}</p>
+          </div>
+          <div className="slot2 shadow">
+            <p><strong>Pergunta</strong>: {contato.pergunta}</p>
+          </div>
+          <div className="slot3 shadow">
+            <p><strong>Nome</strong>: {contato.nome} {contato.sobrenome}</p>
+            <p><strong>email</strong>: {contato.email}</p>
+            <p><strong>Telefone</strong>: {contato.telefone}</p>
+          </div>
         </div>
       ))}
     </div>
